4eb11f40b9a5e0b4f2acd4bd1afe52c4e787b449
Mirek Pawelec <mirek.pawelec@wp.pl>
2018-11-23 01:54:17 +0100

    LI-0002/added button and new fxml window to add ing book

--- src/main/java/pl/biblioteczka/controller/MainController.java
+++ src/main/java/pl/biblioteczka/controller/MainController.java
@@ -1,16 +1,19 @@
 package pl.biblioteczka.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
     @FXML
-    private BorderPane borderPane;
+    private BorderPane rootBorderPane;
 
     @FXML
     private TopMenuButtonsController topMenuButtonsController;
@@ -20,4 +23,16 @@ public class MainController implements Initializable {
         topMenuButtonsController.setMainController(this);
     }
 
+    public void setCenter(String fxmlPath) {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource(fxmlPath));
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
+        Parent parent = null;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        rootBorderPane.setCenter(parent);
+    }
 }
--- src/main/java/pl/biblioteczka/controller/TopMenuButtonsController.java
+++ src/main/java/pl/biblioteczka/controller/TopMenuButtonsController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,22 +20,37 @@ public class TopMenuButtonsController implements Initializable {
     @FXML
     private ToggleButton statisticsButton;
 
+    @FXML
+    private ToggleGroup toggleButtons;
+
     private MainController mainController;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
 
+    @FXML
     public void openLibrary(ActionEvent actionEvent) {
         System.out.println("openLibrary()");
     }
 
+    @FXML
     public void openListBooks(ActionEvent actionEvent) {
         System.out.println("openListBooks()");
     }
 
+    @FXML
     public void openStatistics(ActionEvent actionEvent) {
         System.out.println("openStatistics()");
+    }
+
+    @FXML
+    public void addBook(ActionEvent actionEvent) {
+        if(toggleButtons.getSelectedToggle() != null) {
+            toggleButtons.getSelectedToggle().setSelected(false);
+        }
+
+        mainController.setCenter("/fxml/AddBook.fxml");
     }
 
     public void setMainController(MainController mainController) {
